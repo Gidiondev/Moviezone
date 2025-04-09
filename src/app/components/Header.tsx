ebddca65ee1f66d745d629886f8fc7b2a2882588
gidionnyigana <gidionnyigana@gmail.com>
2025-04-09 20:08:32 +0000

    fetch

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+import {FaHome , FaInfo} from "react-icons/fa"
+import Link from 'next/link';
+
+function Header() {
+  return (
+    <div className='flex justify-between item-center p-3 max-w-6xl mx-auto ' >
+        <div className='flex gap-4'>
+            <MenuItem title='Home' address="/" Icon={FaHome}/>
+            <MenuItem title='About' address="/" Icon={FaInfo}/>
+        </div>
+        <Link href={'/'} className='flex gap-1 item-center '>
+            <span className='text-xl bg-amber-500 font-bold py-1 px-1 rounded-lg'>Movie</span>
+            <span className='text-xl hidden sm:inline'>zone</span>
+        </Link>
+    </div>
+  )
+}
+
+export default Header
